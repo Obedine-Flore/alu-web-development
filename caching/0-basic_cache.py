eeafcb310a6c5f3ae7dc90d12c0d4002fc1f4bf8
Obedine-Flore <f.obedine@alustudent.com>
2024-05-17 20:38:05 +0100

    files updated

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Basic cache"""
 
-class BaseCaching:
-    def __init__(self):
-        self.cache_data = {}
+BaseCaching = __import__('base_caching').BaseCaching
+
+
 class BasicCache(BaseCaching):
-    def __init__(self):
-        super().__init__()
-    
+    """Basic cache"""
+
     def put(self, key, item):
-        """Assign the item value for the key in the dictionary cache_data."""
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value linked to key in cache_data."""
+        """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
