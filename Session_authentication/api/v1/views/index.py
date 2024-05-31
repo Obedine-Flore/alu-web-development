3530b1fd66a7b4fb371d4d1ed75090f1d907c410
Obedine-Flore <f.obedine@alustudent.com>
2024-05-31 23:23:34 +0100

    files added

--- Session_authentication/api/v1/views/index.py
+++ Session_authentication/api/v1/views/index.py
@@ -5,6 +5,24 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def raise_403() -> None:
+    """ GET /api/v1/forbidden
+   Abort:
+      - Raises 403 error to test response
+    """
+    abort(403)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def raise_401() -> None:
+    """ GET /api/v1/unauthorized
+   Abort:
+      - Raises 401 error to test response
+    """
+    abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -24,21 +42,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """
-    GET /api/v1/unauthorized
-    return unauthorized message
-    """
-    abort(401, description="UNAUTHORIZED")
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """
-    GET /api/v1/forbidden
-    return 403 page
-    """
-    abort(403, description="FORBIDDEN")
